ad818da16ce015da0e1d4362ed16eb0a4490e002
alomsoltanul <alomsoltanul@gmail.com>
2023-09-14 18:59:43 +0900

    Components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
+import Header from './components/Header/Header'
 
 function App() {
   
 
   return (
     <>
-      <div>
+      <Header></Header>
+      <div className='md:flex justify-between items-center'>
         <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
       </div>
       
     </>
---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,10 @@
+
+const Bookmarks = () => {
+    return (
+        <div className="md:w-1/3">
+            <h5>Bookmarks</h5>
+        </div>
+    );
+};
+
+export default Bookmarks;
